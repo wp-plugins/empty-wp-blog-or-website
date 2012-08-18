94b4af91ef729c4e809cda8dccedc0e746068746
anoopmmc <anoopmmc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-18 19:24:22 +0000

    git-svn-id: https://plugins.svn.wordpress.org/empty-wp-blog-or-website/trunk@587225 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- empty-wp-blog.php
+++ empty-wp-blog.php
@@ -3,7 +3,7 @@
 Plugin Name: Empty WP Blog
 Plugin URI: http://wordpress.org/extend/plugins/empty-wp-blog-or-website
 Description: One click solution for make your blog/website empty. Delete all your posts, pages, media(images,videos,etc) , tags and categories.
-Version: 1.0
+Version: 1.1
 Author: Anoop M C
 Author Email: anoopmmc@gmail.com
 Author URI: http://fb.com/anoopmc
@@ -19,7 +19,8 @@ function empty_wp_options() {
 		wp_die( __('You do not have sufficient permissions to access this page.') );
 	}
 	if( isset( $_POST['delete_whole'] ) ) {
-
+/*Program Ends Here*/
+if(is_admin()) {
 		echo "<h3>Deleting posts/pages.. please wait...</h3>";
 
 		$options = array(
@@ -41,16 +42,23 @@ function empty_wp_options() {
 				delete_posts( $options );
 			}
 		}
-		echo "
-<h3>Deleting categories.. please wait...</h3>";
+		echo "<h3>Deleting categories.. please wait...</h3>";
 		$cats = get_all_category_ids();
 		foreach( $cats as $cat ) {
 			wp_delete_category( $cat );
 			
 		}
+		echo "<h3>Deleting Users.. please wait...</h3>";
+		
+		$users = get_users();
+    foreach($users as $user) 
+	{
+			if($user->ID==1)
+			{echo '<h3 style="color:red;">User '.$user->user_login.' and email: '.$user->user_email.' can\'t be deleted.</h3>';continue; }
+			wp_delete_user( $user->ID,'' );
+		}
 
-		echo "
-<h3>Deleting tags.. please wait...</h3>";
+		echo "<h3>Deleting tags.. please wait...</h3>";
 		$tags = get_terms( 'post_tag', array( 'hide_empty' => false, 'fields' => 'ids' ) );
 		if ($tags) {
 			foreach($tags as $tag) {
@@ -60,15 +68,34 @@ function empty_wp_options() {
 			}
 		}
 		echo 'Congratulations all your posts, pages, media, tags and categories deleted. Start a fresh blog today!';
+	/*Program Ends Here*/
+	}
+	else
+	{
+	wp_die( 'You are not authorised to perform this action', 'Permission denied' );
+	}
 	}
 	else {
 		echo "
 <h2>Empty WP Blog/Website</h2>";
 		echo "<p>By pressing the button below you agree to delete your blog of pages, posts, attachments, comments, categories, and tags. Thie action can't be restored.<br /><b>Don't press unless you really want to empty your Website/Blog!</b></p>";
-		?><input class="button" type="button" name="button_donate" value="Donate Me" onclick="document.location='https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=anoopmmc%40gmail%2ecom&lc=US&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest'" /><?php 
+		?><?php 
 		echo '<form method="post" onsubmit="return confirm(\'Are you sure?\')">';
  echo '<input id="delete_whole" class="button" type="submit" name="delete_whole" value="Click here to Empty your Website/Blog" />';
- echo '</form>';
+ echo '</form><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>';?>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_donations">
+<input type="hidden" name="business" value="anoopmmc@gmail.com">
+<input type="hidden" name="lc" value="US">
+<input type="hidden" name="amount" value="10.00">
+<input type="hidden" name="currency_code" value="USD">
+<input type="hidden" name="no_note" value="0">
+<input type="hidden" name="currency_code" value="USD">
+<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+<?php
 	}
 }
 
@@ -86,5 +113,4 @@ function delete_posts( $options ) {
 		$posts = get_posts( $options );
 	}
 }
-
 ?>
